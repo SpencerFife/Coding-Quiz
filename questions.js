a8388aad77d61fc51458fcf49b48fe00b465d247
Spencer Fife <scottyfife17@gmail.com>
2019-12-20 21:04:51 -0800

    finished questions in javascript file

--- questions.js
+++ questions.js
@@ -1,51 +1,117 @@
 var questions = [
-    {
-        question: "What does HTML stand for?",
-        answers: {
-            a: "Hypertext Maliable Logarithms",
-            b: "Hypertext Markup Language",
-            c: "Honest Telemetry for Marketable Land",
-            d: "No one knows for sure"
-        },
-        correctAnswer: "Hypertext Markup Language"
-    },
-    {
-        question: "What is a function in programming?",
-        answers: {
-            a: "A social gathering.",
-            b: "",
-            c: "",
-            d: "A set of statements that performs a task or calculates a value."
-        },
-        correctAnswer: ""
-    {
-        question: "",
-        answers: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        correctAnswer: ""
-    },
-    {
-        question: "",
-        answers: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        correctAnswer: ""    
-    },
-    {
-        question: "",
-        answers: {
-            a: "",
-            b: "",
-            c: "",
-            d: ""
-        },
-        correctAnswer: ""
-    },
+  {
+    question:
+      "Which of the following type of variable is visible everywhere in your JavaScript code?",
+    choices: [
+      "global variable",
+      "local variable",
+      "Both of the above",
+      "None of the above"
+    ],
+    answer: "global variable"
+  },
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    question: "Where is the correct place to insert a Javascript?",
+    choices: [
+      "Both the <head> section and the <body> section are correct",
+      "The <head> section",
+      "The <body> section"
+    ],
+    answer: "Both the <head> section and the <body> section are correct"
+  },
+  {
+    question:
+      "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    question: "Which of the following is an advantage of using JavaScript?",
+    choices: [
+      "Less server interaction",
+      "Immedicate feedback to the visitors",
+      "Increased interactivity",
+      "All of the above"
+    ],
+    answer: "All of the above"
+  },
+  {
+    question:
+      "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+    choices: ["last()", "put()", "push()", "None of the above"],
+    answer: "push()"
+  },
+  {
+    question:
+      'What is the correct syntax for referring to an external script called "xxx.js"?',
+    choices: [
+      '<script href="xxx.js">',
+      '<script name="xxx.js">',
+      '<script src="xxx.js">'
+    ],
+    answer: '<script src="xxx.js">'
+  },
+  {
+    question: "Which of the following code creates an object?",
+    choices: [
+      "var book = Object();",
+      "var book = new Object();",
+      "var book = new OBJECT()",
+      "var book = new Book();"
+    ],
+    answer: "var book = new Object();"
+  },
+  {
+    question:
+      "Which of the following function of Boolean object returns a string containing the source of the Boolean object?",
+    choices: ["toSource()", "valueOf()", "toString()", "None of the above"],
+    answer: "toSource()"
+  },
+  {
+    question:
+      "Which of the following function of String object is used to find a match between a regular expression and a string, and to replace the matched substring with a new substring?",
+    choices: ["concat()", "match()", "replace()", "search()"],
+    answer: "replace()"
+  },
+  {
+    question:
+      "Which of the following function of String object creates a string to be displayed as bold as if it were in a <b> tag?",
+    choices: ["anchor()", "big()", "blink()", "bold()"],
+    answer: "bold()"
+  },
+  {
+    question:
+      "Which of the following function of String object causes a string to be displayed as a superscript, as if it were in a <sup> tag?",
+    choices: ["sup()", "small()", "strike()", "sub()"],
+    answer: "sup()"
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<js>", "<script>", "<scripting>", "<javascript>"],
+    answer: "<script>"
+  },
+  {
+    question: 'How do you write "Hello World" in an alert box?',
+    choices: [
+      'alert("Hello World")',
+      'msg("Hello World")',
+      'msgBox("Hello World")',
+      'alertBox("Hello World")'
+    ],
+    answer: 'alert("Hello World")'
+  },
+  {
+    question: "How do you create a function in Javascript?",
+    choices: [
+      "function myFunction()",
+      "function:myFunction()",
+      "function = myFunction()"
+    ],
+    answer: "function myFunction()"
+  }
 ];
